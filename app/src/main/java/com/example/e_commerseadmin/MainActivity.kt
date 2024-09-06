f62bf60e336dcd9c68d5833aeeb1762f1d16bce7
Arnav613Gupta <gupta613arnav@gmail.com>
2024-09-06 10:06:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/e_commerseadmin/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.e_commerseadmin
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+}
